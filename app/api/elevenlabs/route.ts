21a9ff76fbe1c4395afd863f397bd8db52c7cbea
srikanthnani <srikanthnani1202@gmail.com>
2024-09-16 01:38:11 +0530

    feature: elevenlabs realtime text to speech init :sparkles:

---
+++ app/api/elevenlabs/route.ts
@@ -0,0 +1,26 @@
+import { ElevenLabsClient } from "elevenlabs";
+
+export async function POST(req: Request) {
+  const { message, voice } = await req.json();
+
+  const elevenlabs = new ElevenLabsClient({
+    apiKey: process.env.ELEVENLABS_API_KEY,
+  });
+
+  try {
+    const audio = await elevenlabs.generate({
+      voice,
+      model_id: "eleven_turbo_v2",
+      voice_settings: { similarity_boost: 0.5, stability: 0.5 },
+      text: message,
+      stream: true,
+    });
+
+    return new Response(audio as any, {
+      headers: { "Content-Type": "audio/mpeg" },
+    });
+  } catch (error: any) {
+    console.error(error);
+    return Response.json(error, { status: error.statusCode });
+  }
+}
